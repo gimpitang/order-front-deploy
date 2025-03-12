5a1825bd75b4496c420f5efd480be25d8d5bbbc5
gimpitang <dudwodpdy@gmail.com>
2025-03-12 20:10:58 +0900

    fix

--- src/views/MemberLogin.vue
+++ src/views/MemberLogin.vue
@@ -45,15 +45,19 @@ export default{
     },
     methods:{
         async doLogin(){
-            const loginData = {email:this.email, password:this.password};
-            console.log("login start !! ");
-            const response = await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member/doLogin`,loginData);
-            console.log("login 응답 : ", response);
-            const token = response.data.token;
-            const refreshToken = response.data.refreshToken;
-            localStorage.setItem('token',token);
-            localStorage.setItem('refreshToken',refreshToken); 
-            window.location.href = "/";
+            try {
+                const loginData = { email: this.email, password: this.password };
+                console.log("login start !!");
+                const response = await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member/doLogin`, loginData);
+                console.log("login 응답: ", response);
+                const token = response.data.token;
+                const refreshToken = response.data.refreshToken;
+                localStorage.setItem('token', token);
+                localStorage.setItem('refreshToken', refreshToken);
+                window.location.href = "/";
+            } catch (error) {
+                console.error("로그인 중 에러 발생:", error);
+            }
         }
     }
 }</script>
